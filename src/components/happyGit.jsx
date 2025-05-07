a84c6daf7e697d35c4dc4dfcb5a52108a990ebd7
Racheli <rd327743167@sgur.org.il>
2025-05-07 17:16:33 +0300

    fix

--- src/components/happyGit.jsx
+++ src/components/happyGit.jsx
@@ -2,7 +2,7 @@ export const HG = () => {
     return (
         <div className="happy-git">
             <h1>Happy Git</h1>
-            <h2>הגיט הכי שמח</h2>
+            <h2>הגיט הכי שמח!!</h2>
         </div>
     );
 };
